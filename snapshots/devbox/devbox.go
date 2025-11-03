d8d5c394cc98d454765e7f1ac332cf96bf1ba51c
Zllinc <2965202581@qq.com>
2025-11-03 07:12:58 +0000

    fix: lv is wrongly deleted when lv created

--- snapshots/devbox/devbox.go
+++ snapshots/devbox/devbox.go
@@ -867,6 +867,11 @@ func (o *Snapshotter) createSnapshot(ctx context.Context, kind snapshots.Kind, k
 		return nil, err
 	}
 
+	// transaction end, unmark lv
+	if lvName != "" {
+		o.unmarkLV(lvName)
+	}
+
 	return o.mounts(s), nil
 }
 
